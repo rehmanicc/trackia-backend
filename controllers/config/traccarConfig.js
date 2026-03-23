4c0f405e12c6f4fc65caaebbbf81af0d8a26a177
Muhammad Rehman <your-email@gmail.com>
2026-03-23 11:21:10 +0500

    Fix axios instance with basic auth

--- controllers/config/traccarConfig.js
+++ controllers/config/traccarConfig.js
@@ -1,8 +1,18 @@
 const axios = require("axios");
 
+const EMAIL = process.env.TRACCAR_EMAIL;
+const PASSWORD = process.env.TRACCAR_PASSWORD;
+
 const traccarAPI = axios.create({
   baseURL: process.env.TRACCAR_URL,
-  withCredentials: true
+  auth: {
+    username: EMAIL,
+    password: PASSWORD
+  },
+  headers: {
+    Authorization:
+      "Basic " + Buffer.from(`${EMAIL}:${PASSWORD}`).toString("base64")
+  }
 });
 
 module.exports = traccarAPI;
